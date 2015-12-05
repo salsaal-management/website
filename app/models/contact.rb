849b4d81c708c726483f18c17a0f16f9f9f1384c
Mortaza Rezae <rezaemortaza@gmail.com>
2015-12-05 20:16:24 +0800

    minor bug fix

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -6,7 +6,7 @@ class Contact < MailForm::Base
 
   	def headers
   	{
-	  	:subject => "Contact Form",
+	  	:subject => "Salsaal Contact Form",
 	  	:to => "rezaemortaza@gmail.com",
 	  	:from => %("#{name}" <#{email}>)
 	  }
