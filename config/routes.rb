5869ae823bb026c3bfef26a12924b09def29c146
Mortaza Rezae <rezaemortaza@gmail.com>
2015-12-05 19:15:38 +0800

    Formatting

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'pages/about'
-
-  get 'student_branches/curtin_salsaal'
 
   devise_for :users
   resources :posts
@@ -9,11 +6,12 @@ Rails.application.routes.draw do
   
   get 'welcome/index'
   root 'welcome#index'
-  get 'abouts/index'
-
+  
   get 'programs/refugees'
   get 'programs/social_and_cultural'
   get 'programs/women'
+  get 'pages/about'
+  get 'student_branches/curtin_salsaal'
 
 
   get '*path' => redirect('/')
